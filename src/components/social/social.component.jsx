d8a2576de34efb0b1698a829b098eae761d6f1d4
tknieza <tadasknieza@gmail.com>
2019-08-09 19:56:11 +0300

    better auditing

--- src/components/social/social.component.jsx
+++ src/components/social/social.component.jsx
@@ -1,37 +1,15 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import GitHubButton from "react-github-btn";
 import "./social.styles.scss";
 
-export class Social extends PureComponent {
-  componentDidMount() {
-    const script = document.createElement("script");
-    script.src = "https://platform.twitter.com/widgets.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }
-
-  render() {
-    return (
-      <div className="social darkmode-ignore">
-        <GitHubButton
-          href="https://github.com/tknieza"
-          data-size="large"
-          aria-label="Follow @ordoven on GitHub"
-        >
-          Follow
-        </GitHubButton>
-        <a
-          href="https://twitter.com/tadasknieza"
-          className="twitter-follow-button"
-          data-size="large"
-          data-show-screen-name="false"
-          data-lang="en"
-          data-dnt="true"
-          data-show-count="false"
-        >
-          Follow
-        </a>
-      </div>
-    );
-  }
-}
+export const Social = () => (
+  <div className="social darkmode-ignore">
+    <GitHubButton
+      href="https://github.com/tknieza"
+      data-size="large"
+      aria-label="Follow @tknieza on GitHub"
+    >
+      Follow
+    </GitHubButton>
+  </div>
+);
