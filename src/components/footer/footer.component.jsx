aef57210f40c8cbfbd4118d8d5cf416da464257f
tknieza <tadasknieza@gmail.com>
2019-10-24 23:10:48 +0100

    add more footer buttons, change site icon

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,15 +2,28 @@ import React from "react";
 
 import "./footer.styles.scss";
 
+import netlifyImage from "../../assets/netlify.png";
+import reactImage from "../../assets/react.png";
+
 export const Footer = () => {
   return (
     <footer>
       <div className="section">
-        <a href="https://github.com/ordoven">Github</a>
-        <a href="https://twitter.com/tadasknieza">Twitter</a>
-        <a href="https://www.linkedin.com/in/tadas-knieza/">LinkedIn</a>
-        <a href="mailto:tadasknieza@gmail.com">Email</a>
-        <a href="https://github.com/ordoven/tadasknieza.com">View source</a>
+        <div>
+          <a href="https://github.com/tknieza">Github</a>
+          <a href="https://twitter.com/tadasknieza">Twitter</a>
+          <a href="https://www.linkedin.com/in/tadas-knieza/">LinkedIn</a>
+          <a href="mailto:tadasknieza@gmail.com">Email</a>
+          <a href="https://github.com/tknieza/tadasknieza.com">View source</a>
+        </div>
+        <div>
+          <a href="https://reactjs.org/" data-tip="Built with React">
+            <img width="30rem" src={reactImage} alt="React" />
+          </a>
+          <a href="https://netlify.com" data-tip="Hosted by Netlify">
+            <img width="30rem" src={netlifyImage} alt="Netlify" />
+          </a>
+        </div>
       </div>
     </footer>
   );
