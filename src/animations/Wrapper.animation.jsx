495b60b42ff825287cc192a3a069b123d882c380
tknieza <tadasknieza@gmail.com>
2019-11-07 14:17:22 +0000

    add animation wrapper

---
+++ src/animations/Wrapper.animation.jsx
@@ -0,0 +1,12 @@
+import posed from "react-pose";
+
+export const Wrapper = posed.div({
+  visible: { opacity: 1, transition: { duration: 400, ease: "linear" } },
+  hidden: { opacity: 0 },
+  popped: {
+    x: -10,
+    y: -10,
+    opacity: 1,
+    transition: { duration: 700 }
+  }
+});
