9ecfab54902e87ddad48a2f8b209056b3d65a8dd
tknieza <tadasknieza@gmail.com>
2019-11-09 18:38:15 +0000

    add more projects

--- src/components/Projects/projects.component.jsx
+++ src/components/Projects/projects.component.jsx
@@ -4,9 +4,12 @@ import { u1F4BE } from "react-icons-kit/noto_emoji_regular/u1F4BE";
 import { Icon } from "react-icons-kit";
 import Social from "../social/social.component";
 
+import agami from "../../assets/agami.png";
 import flica from "../../assets/flica.svg";
+import kingPixel from "../../assets/kingpixel.svg";
 import selectNote from "../../assets/select-note.svg";
 import tadasknieza from "../../assets/tadasknieza.svg";
+import twitterBird from "../../assets/twitter-bird.svg";
 
 import "./projects.styles.scss";
 
@@ -82,25 +85,84 @@ const Projects = () => {
               desc: "Source code for tadasknieza.com"
             }}
           />
+          <a
+            href="https://tadasknieza.com"
+            className="button"
+            aria-label="Personal website page"
+          >
+            {<Icon size={42} icon={u1F4BE} />}
+          </a>
         </div>
       </div>
       <div className="project">
         <h2>
-          <img src={tadasknieza} alt="tadasknieza.com" />
-          Tadas Knieža
+          <img src={agami} alt="Agami" />
+          Agami
         </h2>
         <p>
-          Personal website featuring dark mode, prop-typing, SASS support and
-          best practices for React (this website's source code).
+          Puzzle game written in C#/Unity. Colors fundamentaly change the world
+          & its obsticles enabling players to solve puzzles.
+        </p>
+        <div className="buttons">
+          <a
+            href="https://ordoven.itch.io/agami"
+            className="button"
+            aria-label="Agami store page"
+          >
+            {<Icon size={42} icon={u1F4BE} />}
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <h2>
+          <img src={kingPixel} alt="King Pixel" />
+          King Pixel
+        </h2>
+        <p>
+          A fully-fledged action-adventure video game featuring advanced level
+          design, player systems, game states. libero!
         </p>
         <div className="buttons">
           <Social
             github={{
-              url: "https://github.com/tknieza/tadasknieza.com",
+              url: "https://github.com/tknieza/king-pixel",
               title: "Source",
-              desc: "Source code for tadasknieza.com"
+              desc: "Source code for King Pixel"
             }}
           />
+          <a
+            href="https://gamejolt.com/games/kingpixel/250736"
+            className="button"
+            aria-label="King Pixel store page"
+          >
+            {<Icon size={42} icon={u1F4BE} />}
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <h2>
+          <img src={twitterBird} alt="Visual Twitter" />
+          Visual Twitter
+        </h2>
+        <p>
+          React Web application which fetches the latest photos posted on
+          Twitter by specifying keywords.
+        </p>
+        <div className="buttons">
+          <Social
+            github={{
+              url: "https://github.com/tknieza/visual-twitter",
+              title: "Source",
+              desc: "Source code for Visual Twitter"
+            }}
+          />
+          <a
+            href="https://github.com/tknieza"
+            className="button"
+            aria-label="Visual Twitter page"
+          >
+            {<Icon size={42} icon={u1F4BE} />}
+          </a>
         </div>
       </div>
     </div>
