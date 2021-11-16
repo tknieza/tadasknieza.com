f4c8615fa6e00b632ed0a94f7d120eb403f21688
tknieza <tadasknieza@gmail.com>
2021-11-16 22:27:47 +0000

    setting up tailwind css in the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.styles.scss";
+import "./tailwind.css";
 import Typography from "typography";
 import githubTheme from "typography-theme-github";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.styles.scss";
 
 new Typography(githubTheme).injectStyles();
 ReactDOM.render(<App />, document.getElementById("root"));
