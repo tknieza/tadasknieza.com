934ed74751a7683d6f533e4951b3bce8f41f398d
tknieza <tadasknieza@gmail.com>
2022-01-30 00:52:10 +0000

    fixed nav popover button styling

--- src/components/popover.jsx
+++ src/components/popover.jsx
@@ -36,17 +36,12 @@ function MyPopover({ toggleColorMode }) {
               className={`
                 ${
                   open
-                    ? 'dark:border-white'
-                    : 'text-opacity-50 dark:border-black'
+                    ? 'bg-brblack border-brblack text-white dark:border-white dark:bg-white dark:text-brblack text-opacity-100 border-opacity-100'
+                    : 'border-black text-black dark:text-white dark:border-white text-opacity-80 border-opacity-80'
                 }
-                text-gray-50 group border-black border-2 px-3 py-2 rounded-md inline-flex items-center text-base font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 transition-colors`}
+              border-2 px-3 py-2 rounded-md inline-flex items-center text-base font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 transition-colors ease-in-out duration-200`}
             >
-              <FiMenu
-                className={`${
-                  open ? '' : 'text-opacity-70'
-                } h-5 w-5 group-hover:text-opacity-90 transition ease-in-out duration-150`}
-                aria-hidden="true"
-              />
+              <FiMenu className="h-5 w-5" aria-hidden="true" />
             </Popover.Button>
             <Transition
               as={Fragment}
