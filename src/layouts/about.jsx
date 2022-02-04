b969d8b0d0510fb997e3bd5186050599528a1c68
tknieza <tadasknieza@gmail.com>
2022-02-04 00:52:18 +0000

    more styling

--- src/layouts/about.jsx
+++ src/layouts/about.jsx
@@ -6,82 +6,81 @@ import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs'
 function About() {
   return (
     <div className="text-brblack dark:text-white">
-      <div>
-        <h1 className="text-2xl font-bold pt-8 pb-3">
-          Me
-          <span className="font-extrabold pl-1">_</span>
-        </h1>
-        <p className="font-blackbird tracking-wide text-justify pb-2">
-          I am an aspiring software engineer that has practical knowledge of
-          building and contributing to software systems. I have been interested
-          in software since high school, where I learnt to code video games and
-          participated in competitive coding competitions. My primary focus
-          nowadays is modern Web application development as, in my opinion,
-          Progressive Web Applications are the future of the modern Web!
-        </p>
+      <h1 className="text-2xl font-bold pt-8 pb-3">
+        Me
+        <span className="font-extrabold pl-1">_</span>
+      </h1>
+      <p className="font-blackbird tracking-wide text-justify pb-2">
+        I am an aspiring software engineer that has practical knowledge of
+        building and contributing to software systems. I have been interested in
+        software since high school, where I learnt to code video games and
+        participated in competitive coding competitions. My primary focus
+        nowadays is modern Web application development as, in my opinion,
+        Progressive Web Applications are the future of the modern Web!
+      </p>
 
-        <p className="font-blackbird tracking-wide text-justify pb-2">
-          Recently, I have started uploading and version-controlling my software
-          projects like{' '}
-          <a
-            className="dark:text-electric-yellow text-red-400 transition-colors"
-            href="https://sleepy-joliot-a28cc4.netlify.com"
-          >
-            Flica
-          </a>{' '}
-          &{' '}
-          <a
-            className="dark:text-electric-yellow text-red-400 transition-colors"
-            href="https://sharp-brahmagupta-cd3ffa.netlify.com/"
-          >
-            Select Note
-          </a>{' '}
-          on Github.
-        </p>
+      <p className="font-blackbird tracking-wide text-justify pb-2">
+        Recently, I have started uploading and version-controlling my software
+        projects like{' '}
+        <a
+          className="dark:text-electric-yellow text-red-400 transition-colors"
+          href="https://sleepy-joliot-a28cc4.netlify.com"
+        >
+          Flica
+        </a>{' '}
+        &{' '}
+        <a
+          className="dark:text-electric-yellow text-red-400 transition-colors"
+          href="https://sharp-brahmagupta-cd3ffa.netlify.com/"
+        >
+          Select Note
+        </a>{' '}
+        on Github.
+      </p>
 
-        <p className="font-blackbird tracking-wide text-justify pb-2">
-          These projects utilize external APIs, databases and front-end web
-          frameworks. As a Newcastle University computer science student, I have
-          achieved a first-class honours average through stage 1 of my studies.
-        </p>
+      <p className="font-blackbird tracking-wide text-justify pb-2">
+        These projects utilize external APIs, databases and front-end web
+        frameworks. As a Newcastle University computer science student, I have
+        achieved a first-class honours average through stage 1 of my studies.
+      </p>
 
-        <h1 className="text-2xl font-bold py-3">
-          Around the Web
-          <span className="font-extrabold pl-1">_</span>
-        </h1>
+      <h1 className="text-2xl font-bold py-3">
+        Around the Web
+        <span className="font-extrabold pl-1">_</span>
+      </h1>
 
-        <ul>
-          <li>
-            <LinkButton
-              url="mailto:tadasknieza@gmail.com"
-              title="Email"
-              Icon={MdEmail}
-            />
-          </li>
-          <li>
-            <LinkButton
-              url="https://www.linkedin.com/in/tadas-knieza/"
-              title="LinkedIn"
-              Icon={BsLinkedin}
-            />
-          </li>
-          <li>
-            <LinkButton
-              url="https://github.com/tknieza"
-              title="GitHub"
-              Icon={BsGithub}
-            />
-          </li>
-          <li>
-            <LinkButton
-              url="https://twitter.com/tadasknieza"
-              title="Twitter"
-              Icon={BsTwitter}
-            />
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <LinkButton
+            url="mailto:tadasknieza@gmail.com"
+            title="Email"
+            Icon={MdEmail}
+          />
+        </li>
+        <li>
+          <LinkButton
+            url="https://www.linkedin.com/in/tadas-knieza/"
+            title="LinkedIn"
+            Icon={BsLinkedin}
+          />
+        </li>
+        <li>
+          <LinkButton
+            url="https://github.com/tknieza"
+            title="GitHub"
+            Icon={BsGithub}
+          />
+        </li>
+        <li>
+          <LinkButton
+            url="https://twitter.com/tadasknieza"
+            title="Twitter"
+            Icon={BsTwitter}
+          />
+        </li>
+      </ul>
 
-        {/* <h1>Music</h1>
+      {/* <h1>Music</h1>
         <p>
           I like to record/produce music in my spare time. Here are some
           tracks.
@@ -101,78 +100,77 @@ function About() {
           </li>
         </ul> */}
 
-        <h1 className="text-2xl font-bold py-3">
-          Settings &amp; Gear
-          <span className="font-extrabold pl-1">_</span>
-        </h1>
+      <h1 className="text-2xl font-bold py-3">
+        Settings &amp; Gear
+        <span className="font-extrabold pl-1">_</span>
+      </h1>
 
-        <ul className="font-blackbird">
-          <li className="flex">
-            <span className="w-16">Main PC</span>
-            <ul>
-              <li className="flex">
-                <span className="w-16">CPU</span>
-                <p>AMD Ryzen 5 2600X</p>
-              </li>
-              <li className="flex">
-                <span className="w-16">GPU</span>
-                <p>Nvidia GeForce GTX 1660 SUPER</p>
-              </li>
-              <li className="flex">
-                <span className="w-16">MOBO</span>
-                <p>ASRock B450M Pro4</p>
-              </li>
-              <li className="flex">
-                <span className="w-16">Memory</span>
-                <p>G.Skill Aegis 16 GB (2 x 8GB)</p>
-              </li>
-              <li className="flex">
-                <span className="w-16">Storage</span>
-                <p>ADATA XPG GAMMIX S11 Pro (512 GB)</p>
-              </li>
-              <li className="flex">
-                <span className="w-16">Cooler</span>
-                <p>Cooler Master Hyper 212 Black Edition</p>
-              </li>
-            </ul>
-          </li>
-          <li className="flex">
-            <span className="w-16">Laptop</span> <p>ASUS Zenbook UX430UQ</p>
-          </li>
-          <li className="flex">
-            <span className="w-16">Editor</span>
-            <p>Visual Studio Code</p>
-          </li>
+      <ul className="font-blackbird">
+        <li className="flex">
+          <span className="w-16">Main PC</span>
+          <ul>
+            <li className="flex">
+              <span className="w-16">CPU</span>
+              <p>AMD Ryzen 5 2600X</p>
+            </li>
+            <li className="flex">
+              <span className="w-16">GPU</span>
+              <p>Nvidia GeForce GTX 1660 SUPER</p>
+            </li>
+            <li className="flex">
+              <span className="w-16">MOBO</span>
+              <p>ASRock B450M Pro4</p>
+            </li>
+            <li className="flex">
+              <span className="w-16">Memory</span>
+              <p>G.Skill Aegis 16 GB (2 x 8GB)</p>
+            </li>
+            <li className="flex">
+              <span className="w-16">Storage</span>
+              <p>ADATA XPG GAMMIX S11 Pro (512 GB)</p>
+            </li>
+            <li className="flex">
+              <span className="w-16">Cooler</span>
+              <p>Cooler Master Hyper 212 Black Edition</p>
+            </li>
+          </ul>
+        </li>
+        <li className="flex">
+          <span className="w-16">Laptop</span> <p>ASUS Zenbook UX430UQ</p>
+        </li>
+        <li className="flex">
+          <span className="w-16">Editor</span>
+          <p>Visual Studio Code</p>
+        </li>
 
-          <li className="flex">
-            <span className="w-16">Theme</span>
-            <a
-              className="hover:underline underline-offset-2"
-              href="https://github.com/sdras/night-owl-vscode-theme"
-            >
-              Night Owl
-            </a>
-          </li>
-          <li className="flex">
-            <span className="w-16">Config</span>
-            <a
-              className="hover:underline underline-offset-2"
-              href="https://google.com/"
-            >
-              Repo Link
-            </a>
-          </li>
-          <li className="flex">
-            <span className="w-16">Notes</span>
-            <a
-              className="hover:underline underline-offset-2"
-              href="https://products.office.com/en-gb/onenote/digital-note-taking-app/"
-            >
-              Microsoft OneNote
-            </a>
-          </li>
-        </ul>
-      </div>
+        <li className="flex">
+          <span className="w-16">Theme</span>
+          <a
+            className="hover:underline underline-offset-2"
+            href="https://github.com/sdras/night-owl-vscode-theme"
+          >
+            Night Owl
+          </a>
+        </li>
+        <li className="flex">
+          <span className="w-16">Config</span>
+          <a
+            className="hover:underline underline-offset-2"
+            href="https://google.com/"
+          >
+            Repo Link
+          </a>
+        </li>
+        <li className="flex">
+          <span className="w-16">Notes</span>
+          <a
+            className="hover:underline underline-offset-2"
+            href="https://products.office.com/en-gb/onenote/digital-note-taking-app/"
+          >
+            Microsoft OneNote
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/layouts/books.jsx
+++ src/layouts/books.jsx
@@ -1,62 +1,70 @@
 import React from 'react'
 
+const books = [
+  {
+    author: 'Steven Pressfield',
+    name: 'The War of Art',
+    year: '2011'
+  },
+  {
+    author: 'John Sonmez',
+    name: "Soft Skills: The software developer's life manual",
+    year: '2014'
+  },
+  {
+    author: 'Steven McConnell',
+    name: 'Code Complete: A Practical Handbook of Software Construction',
+    year: '2004'
+  },
+  {
+    author: 'Gayle Laakmann McDowell',
+    name: 'Cracking the Coding Interview',
+    year: '2011'
+  },
+  {
+    author: 'Andrew Hunt/David Thomas',
+    name: 'The Pragmatic Programmer',
+    year: '2000'
+  },
+  {
+    author: 'Douglas Adams',
+    name: "The Hitchhiker's Guide to the Galaxy",
+    year: '2007'
+  },
+  {
+    author: 'Sun Tzu',
+    name: 'The Art of War',
+    year: '2009'
+  },
+  {
+    author: 'Bruce Eckel',
+    name: 'Thinking in Java',
+    year: '2006'
+  },
+  {
+    author: 'Douglas Crockford',
+    name: 'JavaScript - The Good Parts',
+    year: '2008'
+  }
+]
+
 function Books() {
   return (
-    <div>
-      <div>
-        <div className="section">
-          <h1>Books</h1>
-          <p>Keeping track since 2019</p>
-        </div>
-        <div className="section">
-          <h2>To Be Finished</h2>
-          <ul>
-            <li>
-              Steven Pressfield, Shawn Coyne, <b>The War of Art</b>, Black Irish
-              Entertainment LLC, 2011.
-            </li>
-            <li>
-              John Sonmez,{' '}
-              <b>Soft Skills: The software developer&apos;s life manual</b>,
-              Manning Publications, 2014.
-            </li>
-            <li>
-              Steven McConnell,{' '}
-              <b>
-                Code Complete: A Practical Handbook of Software Construction
-              </b>
-              , Microsoft Press, 2004.
-            </li>
-            <li>
-              Gayle Laakmann McDowell, <b>Cracking the Coding Interview</b>,
-              CareerCup, 2011.
-            </li>
-          </ul>
-        </div>
-        <div className="section">
-          <h2>Read</h2>
-          <ul>
-            <li>
-              Andrew Hunt and David Thomas, <b>The Pragmatic Programmer</b>,
-              Addison-Wesley, 2000.
-            </li>
-            <li>
-              Douglas Adams, <b>The Hitchhiker&apos;s Guide to the Galaxy</b>,
-              Penguin Random House USA, 2007.
-            </li>
-            <li>
-              Sun Tzu, <b>The Art of War</b>, Pax Librorum, 2009.
-            </li>
-            <li>
-              Bruce Eckel, <b>Thinking in Java</b>, Prentice Hall, 2006.
-            </li>
-            <li>
-              Douglas Crockford, <b>JavaScript - The Good Parts</b>, Yahoo
-              Press, 2008.
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div className="text-brblack dark:text-white">
+      <h1 className="text-2xl font-bold pt-8 pb-3">
+        Book Recommendations
+        <span className="font-extrabold pl-1">_</span>
+      </h1>
+      <ul className="font-blackbird">
+        {books.map(item => (
+          <li key={item.name} className="flex justify-between">
+            <span className="w-56">{item.author}</span>
+            <p className="text-right">
+              {item.name}, {item.year}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
